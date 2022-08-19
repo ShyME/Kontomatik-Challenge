4d9bfe41abd050943b73f113fa5be5b7b2e563b9
ShyME <shyme212@gmail.com>
2022-08-19 17:26:48 +0200

    removed commented out code

--- src/main/java/me/imshy/bankingInfo/adapters/general/credentialsReader/CredentialsStdInReader.java
+++ src/main/java/me/imshy/bankingInfo/adapters/general/credentialsReader/CredentialsStdInReader.java
@@ -6,17 +6,6 @@ import java.util.Scanner;
 
 public class CredentialsStdInReader {
 
-  /*public static Credentials readCredentials() {
-    // Cannot invoke "java.io.Console.readLine()" because the return value of "java.lang.System.console()" is null
-    System.out.print("PKO login: ");
-    String login = System.console().readLine();
-
-    System.out.print("PKO password: ");
-    String password = String.valueOf(System.console().readPassword());
-
-    return new Credentials(login, password);
-  }*/
-
   public static Credentials readCredentials() {
     Scanner scanner = new Scanner(System.in);
 
