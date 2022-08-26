c421abf0d8e3a389c68429ecfa5408dfce19f11e
ShyME <shyme212@gmail.com>
2022-08-26 11:32:06 +0200

    moved error description parsing to its client

--- src/main/java/me/imshy/bankingInfo/domain/pko/http/util/Requests.java
+++ src/main/java/me/imshy/bankingInfo/domain/pko/http/util/Requests.java
@@ -1,16 +1,10 @@
 package me.imshy.bankingInfo.domain.pko.http.util;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import me.imshy.bankingInfo.domain.general.http.request.JsonPostRequest;
-import me.imshy.bankingInfo.domain.general.http.request.Response;
 import me.imshy.bankingInfo.domain.pko.http.SessionAttributes;
 import me.imshy.bankingInfo.domain.pko.http.body.InitRequestBody;
 import me.imshy.bankingInfo.domain.pko.http.body.LoginRequestBody;
 import me.imshy.bankingInfo.domain.pko.http.body.PasswordRequestBody;
-import me.imshy.bankingInfo.infrastructure.general.util.JsonUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Requests {
   private static final String PKO_BASE_URL = "https://www.ipko.pl/ipko3";
@@ -36,31 +30,5 @@ public class Requests {
         .setBody(new InitRequestBody())
         .addHeader(SESSION_ID_HEADER_NAME, sessionId)
         .build();
-  }
-
-  public static List<String> getErrorDescriptions(Response response) {
-    List<JsonNode> errorJsons = JsonUtils.findValuesByKey("errors", response.body());
-    List<String> errors = new ArrayList<>();
-    for (JsonNode errorNode : errorJsons) {
-      if (errorNode.isArray()) {
-        for (int i = 0; i < errorNode.size(); i++) {
-          JsonNode element = errorNode.get(i);
-          errors.addAll(getErrorDescriptions(element));
-        }
-      } else {
-        errors.addAll(getErrorDescriptions(errorNode));
-      }
-    }
-    return errors;
-  }
-
-  private static List<String> getErrorDescriptions(JsonNode errorNode) {
-    List<String> errors = new ArrayList<>();
-    errorNode.elements().forEachRemaining(element -> {
-      if (!element.textValue().equals("")) {
-        errors.add(element.textValue());
-      }
-    });
-    return errors;
   }
 }
