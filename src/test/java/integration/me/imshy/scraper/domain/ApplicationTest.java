8e92f109bd71f9ff6f7d6a75c829b53f2c634d15
ShyME <shyme212@gmail.com>
2022-09-01 13:30:10 +0200

    extracted StdIoStub from ApplicationTest

--- src/test/java/integration/me/imshy/scraper/domain/ApplicationTest.java
+++ src/test/java/integration/me/imshy/scraper/domain/ApplicationTest.java
@@ -6,9 +6,6 @@ import me.imshy.scraper.domain.exception.InvalidCredentials;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-
 class ApplicationTest {
 
   @Test
@@ -25,47 +22,6 @@ class ApplicationTest {
     StdIOStub.input(new Credentials(credentials.login(), "badPassword"));
     Assertions.assertThatThrownBy(Application::main)
         .isInstanceOf(InvalidCredentials.class);
-  }
-
-  private static class StdIOStub {
-    private static final InputStream DEFAULT_SYSTEM_IN = System.in;
-    private static final PrintStream DEFAULT_SYSTEM_OUT = System.out;
-    private static InputStream inputStream;
-    private static OutputStream outputStream;
-
-    static void input(Credentials credentials) {
-      String input = credentials.login() + System.getProperty("line.separator")
-          + credentials.password() + System.getProperty("line.separator");
-      inputStream = new ByteArrayInputStream(input.getBytes());
-      System.setIn(inputStream);
-    }
-
-    static void around(Runnable action) {
-      StdIOStub.startCapturingOutput();
-      action.run();
-      StdIOStub.resetStreams();
-    }
-
-    private static void startCapturingOutput() {
-      outputStream = new ByteArrayOutputStream();
-      System.setOut(new PrintStream(outputStream, false, StandardCharsets.UTF_8));
-    }
-
-    private static void resetStreams() {
-      try {
-        inputStream.close();
-        outputStream.close();
-        System.setIn(DEFAULT_SYSTEM_IN);
-        System.setOut(DEFAULT_SYSTEM_OUT);
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
-    }
-
-    public static String getOutput() {
-      return outputStream.toString();
-    }
-
   }
 
 }
---
+++ src/test/java/integration/me/imshy/scraper/domain/StdIOStub.java
@@ -0,0 +1,47 @@
+package integration.me.imshy.scraper.domain;
+
+import me.imshy.scraper.domain.Credentials;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class StdIOStub {
+  private static final InputStream DEFAULT_SYSTEM_IN = System.in;
+  private static final PrintStream DEFAULT_SYSTEM_OUT = System.out;
+  private static InputStream inputStream;
+  private static OutputStream outputStream;
+
+  static void input(Credentials credentials) {
+    String input = credentials.login() + System.getProperty("line.separator")
+        + credentials.password() + System.getProperty("line.separator");
+    inputStream = new ByteArrayInputStream(input.getBytes());
+    System.setIn(inputStream);
+  }
+
+  static void around(Runnable action) {
+    StdIOStub.startCapturingOutput();
+    action.run();
+    StdIOStub.resetStreams();
+  }
+
+  private static void startCapturingOutput() {
+    outputStream = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outputStream, false, StandardCharsets.UTF_8));
+  }
+
+  private static void resetStreams() {
+    try {
+      inputStream.close();
+      outputStream.close();
+      System.setIn(DEFAULT_SYSTEM_IN);
+      System.setOut(DEFAULT_SYSTEM_OUT);
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  public static String getOutput() {
+    return outputStream.toString();
+  }
+
+}
